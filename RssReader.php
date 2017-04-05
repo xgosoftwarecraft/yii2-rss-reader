72f3211a8a6d0e9b7c287283f4ac7af1fe50d006
Freixedelo <ruifreixedelo@hotmail.com>
2017-04-05 19:00:48 +0100

    main implementations

--- RssReader.php
+++ RssReader.php
@@ -9,11 +9,26 @@ class RssReader {
      */
     private $sources;
 
+    /**
+     * @var array
+     */
+    public $items = array();
+
     public function read($sources) {
+        $this->sources = $sources;
 
         foreach ($sources as $sc) {
+            $xml = simplexml_load_file($sc);
 
+            echo '<pre>';
+
+            foreach ($xml->xpath('//item') as $item) {
+                array_push($this->items, $item);
+            }
         }
+        usort($this->items, function($a, $b) {
+            return strtotime($b->pubDate) - strtotime($a->pubDate);
+        });
     }
 
 }
