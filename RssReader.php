09117db8a1e4390a7adbd70b3988bd131acb87e1
Freixedelo <ruifreixedelo@hotmail.com>
2017-04-05 14:24:02 +0100

    composer fix

--- RssReader.php
+++ RssReader.php
@@ -1,6 +1,19 @@
+<?php
+
 namespace xgosoftwarecraft\RssFeed;
 
-
 class RssReader {
-	
+
+    /**
+     * @var array
+     */
+    private $sources;
+
+    public function read($sources) {
+
+        foreach ($sources as $sc) {
+
+        }
+    }
+
 }
